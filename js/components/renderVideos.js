a2b648408e13b410f2a2fc3cb2b027f5c196769f
HOLYdave_studios <HOLYdave.com>
2026-07-13 16:59:39 +0100

    create video data layer

--- js/components/renderVideos.js
+++ js/components/renderVideos.js
@@ -1,5 +1,16 @@
 import {videos} from '../data/videos.js';
 
 export function renderVideos (video) {
-  
+  const videoGrid = document.querySelector('.video-grid');
+  let videoRender = "";
+
+  videos.forEach((video) => {
+    videoRender += `
+      <div class="video-card">
+        ${video.title}
+      </div>
+    `;
+  });
+
+  videoGrid.innerHTML = videoRender;
 };
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import './sidebar.js';
 import './videotag.js';
 import { videos } from './data/videos.js';
-import { renderVideos } from './components/renderVideos.js';
+
 
 videos.forEach((video) => { 
   console.log(video.title);
 });
+
+renderVideos();
