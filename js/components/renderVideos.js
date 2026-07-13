a220053e043bb3eb7975e1c057de7e36d2880e30
HOLYdave_studios <HOLYdave.com>
2026-07-13 17:56:04 +0100

    Render all cards from videos.js using forEach.

--- js/components/renderVideos.js
+++ js/components/renderVideos.js
@@ -6,9 +6,39 @@ export function renderVideos (video) {
 
   videos.forEach((video) => {
     videoRender += `
-      <div class="video-card">
-        ${video.title}
-      </div>
+      <article class="video-card">
+        <div class="video-card__thumbnail">
+          <a href="#">
+            <img
+              loading="lazy"
+              src="${video.thumbnail}"
+              alt="${video.title}"
+            />
+          </a>
+          <div class="video-card__time">${video.duration}</div>
+        </div>
+        <div class="video-card__info">
+          <div class="video-card__avatar">
+            <a href="#">
+              <img
+                loading="lazy"
+                src="${video.avatar}"
+                alt="${video.author}"
+              />
+            </a>
+          </div>
+          <div class="video-card__details">
+            <h3 class="video-card__title">
+              ${video.title}
+            </h3>
+            <a class="video-card__author" href="#">${video.author}</a>
+            <p class="video-card__stats">${video.views} views &#183; ${video.uploaded}</p>
+          </div>
+          <div class="video-card__menu">
+            <i class="material-icons">more_vert</i>
+          </div>
+        </div>
+      </article>
     `;
   });
 
