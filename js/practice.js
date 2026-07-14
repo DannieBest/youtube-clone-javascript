7f56dd39dfa9a627562130b7f06e1eee9765eb6d
HOLYdave_studios <HOLYdave.com>
2026-07-14 20:28:18 +0100

    Practice sorting arrays of objects by string properties

--- js/practice.js
+++ js/practice.js
@@ -4,4 +4,17 @@ numbers.sort((a, b) => {
   return b - a;
 });
 
-console.log(numbers)
+const people = [
+    { name: "Dannie" },
+    { name: "Messi" },
+    { name: "Alex" },
+    { name: "Brian" }
+];
+
+console.log(people)
+
+people.sort((a, b) => {
+  return "Dannie".localeCompare("Brian");
+});
+
+console.log(people)
